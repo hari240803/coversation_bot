3124591943d37819f21fabcd43c2af043956cb19
Hareendra sri sai <maddipatihareendra@gmail.com>
2025-11-22 11:58:11 +0530

    updated functions

--- chatbot_frontend.py
+++ chatbot_frontend.py
@@ -2,14 +2,14 @@
 
 #1 import streamlit and chatbot file
 import streamlit as st 
-import  chatbot_backend as demo  #**Import your Chatbot file as demo
+import  chatbot_backend as conv  #**Import your Chatbot file as demo
 
 #2 Set Title for Chatbot - https://docs.streamlit.io/library/api-reference/text/st.title
 st.title("Hi, This is Chatbot Anisha :sunglasses:") # **Modify this based on the title you want in want
 
 #3 LangChain memory to the session cache - Session State - https://docs.streamlit.io/library/api-reference/session-state
 if 'memory' not in st.session_state: 
-    st.session_state.memory = demo.demo_memory() #** Modify the import and memory function() attributes initialize the memory
+    st.session_state.memory = conv.memory() #** Modify the import and memory function() attributes initialize the memory
 
 #4 Add the UI chat history to the session cache - Session State - https://docs.streamlit.io/library/api-reference/session-state
 if 'chat_history' not in st.session_state: #see if the chat history hasn't been created yet
@@ -30,7 +30,7 @@ if input_text:
     
     st.session_state.chat_history.append({"role":"user", "text":input_text}) 
 
-    chat_response = demo.demo_conversation(input_text=input_text, memory=st.session_state.memory) #** replace with ConversationChain Method name - call the model through the supporting library
+    chat_response = conv.conversation(input_text=input_text, memory=st.session_state.memory) #** replace with ConversationChain Method name - call the model through the supporting library
     
     with st.chat_message("assistant"): 
         st.markdown(chat_response) 
